0a6c8e3cca04bb4ec729a1a1091dec1b5c959b85
lysice <snowist@126.com>
2022-05-23 16:31:07 +0800

    [feat] job

---
+++ src/Jobs/VisitSyncJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Lysice\Visits\Jobs;
+
+use App\Service\RequestService;
+use App\Util\HttpClient;
+use App\Util\ServiceClient;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class VisitSyncJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $call = config('laravel-visit.syncCount');
+        return $call($this->data);
+    }
+}
